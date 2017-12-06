0709565939aa27e670e2efe188929c0384118309
David.Rz.Liu <mania7539@gmail.com>
2017-12-06 15:37:33 +0800

    implementing reducer-posts with lodash and mapKeys function: 126

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import { FETCH_POSTS } from "../actions/index";
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            console.log(action.payload.data);
+            return _.mapKeys(action.payload.data, "id");
+            // our source: [post1, post2]
+            // our goal: {4: post1, 36: post2}
+
+        default:
+            return state;
+    }
+
+}
