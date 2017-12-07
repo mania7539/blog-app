b3a9768b0ee2abd8238f2369e60acec189f759c4
David.Rz.Liu <mania7539@gmail.com>
2017-12-08 00:42:30 +0800

    map state property with 2nd argument 'ownProps' for connect component, and get route path wildcard with this.props.match.params: 146

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        // the above code is a destructing format of below code:
+        // ```this.props.match.params.id;```
+        //
+        // the property of 'props' right here is directly provided by react-router.
+        //
+        // now with the react-router the 'match' right here is the top level property,
+        // the 'params' property inside of it is an object that lists all the different wildcard tokens 
+        //  that exists inside the URL. (which means ':id' here, defined in Route path of ./src/index.js)
+        //
+        this.props.fetchPost(id);
+    }
+
     render() {
         return (
             <div>
@@ -10,4 +27,21 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+    return ({
+        post: posts[ownProps.match.params.id]
+    });
+}
+// to mapStateToProps: this.props === ownProps
+// 
+// try to refactor the below code to be a more reusable one as the above 
+// with the 2nd argument.
+// ```
+// function mapStateToProps({ posts }) {
+//     return ({
+//         posts
+//     });
+// }
+// ```
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
