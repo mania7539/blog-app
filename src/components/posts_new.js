5b60496f57080262f955e217cf1615efe00d167f
David.Rz.Liu <mania7539@gmail.com>
2017-12-07 12:46:46 +0800

    setting up redux-form with Field component part2: 134

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,13 +2,44 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div>
+                <input
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+    // the argument 'field' is an object contains some event handlers that we need to wire up
+    // to the JSX that we're returning. 
+    //
+    // the purpose of the argument 'field' is like a callback of Field component.
+    // (or we call the 'field' argument object as bunch of event handlers such as
+    //  onChange, onBlur, onFocus, or etc...)
+    // ```
+    //  <input
+    //     onChange={field.input.onChange}
+    //     onFocus={field.input.onFocus}
+    //     onBlur={field.input.onBlur}
+    //     {...field.input}
+    // />
+    // ```
+    //
+    // it's up to us to make sure that this Field component recognizes that when it calls renderTitleField,
+    // which returns the input component, the Field component needs to be responsible for 
+    // handling any changes of this input.
+    //
+    // NOTE: this function is for the 'component' props in Field component.
+
     render() {
         return (
             <div>
                 <form>
                     <Field
                         name="title"
-                        component={}
+                        component={this.renderTitleField}
                     />
                 </form>
             </div>
@@ -17,6 +48,12 @@ class PostsNew extends Component {
         // the 1st property (name) of the field is meant to reflect the "title" of a post
         // the 2nd property (component) of the field takes in a function, so a function or 
         //  another component that will be used to display this field component.
+        //
+        // we do nothing to set the input data type such as: text, checkbox, or etc in Field.
+        //
+        // ```component={this.renderTitleField}```
+        // we're not putting any parentheses here, we are just providing a reference to a 
+        // function because the field will call that function at some point in the future
     }
 }
 
