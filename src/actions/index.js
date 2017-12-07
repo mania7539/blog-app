51f8012234af3bfb3ce3316edfab71c5e82bbee3
David.Rz.Liu <mania7539@gmail.com>
2017-12-08 00:01:13 +0800

    receiving new post by setup fetchPost action to reducer - fix typo: 145

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const FETCH_POSTS = "fetch_post";
+export const FETCH_POST = "fetch_post";
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POST = "create_post";
 
